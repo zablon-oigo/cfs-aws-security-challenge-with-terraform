693ce8a3d8eb8d2b4908fe70c2f0870c6ce74365
zablon <zablonoigo51@gmail.com>
2024-09-23 13:34:17 +0300

    Associating route table with nat gateway

--- vpc/nat.tf
+++ vpc/nat.tf
@@ -9,3 +9,28 @@ resource "aws_nat_gateway" "Test_NAT" {
     Name = "NAT Gateway"
   }
 }
+resource "aws_route_table" "NAT_Gateway_rt" {
+  vpc_id = aws_vpc.Test_VPC.id
+
+  tags = {
+    Name = "Route table for NAT gateway"
+  }
+}
+
+resource "aws_route" "NAT_Gateway_rt_route" {
+  route_table_id         = aws_route_table.NAT_Gateway_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.Test_NAT.id
+}
+
+resource "aws_route_table_association" "NAT_Gateway_RT_Association_A" {
+  depends_on     = [aws_route_table.NAT_Gateway_rt]
+  subnet_id      = aws_subnet.Private_Subnet_A.id
+  route_table_id = aws_route_table.NAT_Gateway_rt.id
+}
+
+resource "aws_route_table_association" "NAT_Gateway_RT_Association_B" {
+  depends_on     = [aws_route_table.NAT_Gateway_rt]
+  subnet_id      = aws_subnet.Private_Subnet_B.id
+  route_table_id = aws_route_table.NAT_Gateway_rt.id
+}
