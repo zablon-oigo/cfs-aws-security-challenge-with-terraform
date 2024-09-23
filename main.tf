f03f24cb3182869ebd5825c3804e7281228c37ca
zablon <zablonoigo51@gmail.com>
2024-09-23 10:16:59 +0300

    Added route table

--- main.tf
+++ main.tf
@@ -55,3 +55,25 @@ resource "aws_internet_gateway" "Test_VPC_IGW" {
     Name = "test-VPC-IGW"
   }
 }
+resource "aws_route_table" "Test_VPC_rtb" {
+  vpc_id = aws_vpc.Test_VPC.id
+
+  tags = {
+    Name = "test-VPC-rtb"
+  }
+}
+resource "aws_route" "Test_rt" {
+  route_table_id         = aws_route_table.Test_VPC_rtb.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.Test_VPC_IGW.id
+}
+
+resource "aws_route_table_association" "Test_VPC_rtb_assoc1" {
+  subnet_id      = aws_subnet.Public_Subnet_A.id
+  route_table_id = aws_route_table.Test_VPC_rtb.id
+}
+
+resource "aws_route_table_association" "Test_VPC_rtb_assoc2" {
+  subnet_id      = aws_subnet.Public_Subnet_B.id
+  route_table_id = aws_route_table.Test_VPC_rtb.id
+}
