368a3413ded9e035bc813f7b2fb27228b82c9edc
zablon <zablonoigo51@gmail.com>
2024-09-23 13:35:20 +0300

    Added alb,launch template and cloudfront module

--- main.tf
+++ main.tf
@@ -8,3 +8,21 @@ provider "aws" {
 module "test-vpc" {
     source = "./vpc"
 }
+module "launch_template" {
+    source = "./template"
+    security_group_id = module.test-vpc.security_group_id
+}
+module "alb" {
+    source = "./alb"
+    vpc_id = module.test-vpc.vpc_id
+    aws_launch_templates = module.launch_template.launch_template_id
+    subnet_ids_frontend   = module.test-vpc.public_subnet_ids
+    subnet_ids_backend    = module.test-vpc.private_subnet_ids
+    launch_template_id_frontend = module.launch_template.launch_template_id[0]  
+    launch_template_id_backend  = module.launch_template.launch_template_id[1]
+}
+module "cloudFront" {
+    source = "./cloudfront"
+    load_balancer_dns = module.alb.load_balancer_dns
+    
+}
