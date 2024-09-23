94f6f58659d0aab50a01101d3ff760d7ac07055c
zablon <zablonoigo51@gmail.com>
2024-09-23 14:33:08 +0300

    Added vpc security groups

---
+++ vpc/security-groups.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "Test_SG" {
+  name   = "test-SG"
+  vpc_id = aws_vpc.Test_VPC.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
