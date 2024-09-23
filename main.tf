e61ab7299e579cc77b99432b4f2bb1cf220d5c95
zablon <zablonoigo51@gmail.com>
2024-09-23 10:11:53 +0300

    Custom vpc creation

--- main.tf
+++ main.tf
@@ -4,3 +4,9 @@ provider "aws" {
     secret_key = "${var.secret_key}"
     token = "${var.token}"
 }
+resource "aws_vpc" "Test_VPC" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Test-VPC"
+  }
+}
