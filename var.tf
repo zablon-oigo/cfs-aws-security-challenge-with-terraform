1fbe17d6f379ca7e37266e0c628b87619290a7e7
zablon <zablonoigo51@gmail.com>
2024-09-23 10:06:54 +0300

    Initial commit

---
+++ var.tf
@@ -0,0 +1,14 @@
+variable "access_key" {
+    description = "Access key"
+  
+}
+variable "region" {
+    description = "Default Region"
+}
+variable "secret_key" {
+    description = "Secret key"
+  
+}
+variable "token" {
+    description = "token"
+}
