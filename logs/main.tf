4275c47e80d643f3ebea865443b6852bda4e28c6
zablon <zablonoigo51@gmail.com>
2024-09-24 13:27:57 +0300

    Added vpc logs

---
+++ logs/main.tf
@@ -0,0 +1,7 @@
+resource "aws_flow_log" "vpc_flow_log" {
+  vpc_id = var.vpc_id 
+  traffic_type = "ALL"
+  log_destination_type = "s3"
+  log_destination = var.bucket_name.arn
+  iam_role_arn = var.iam_role.arn
+}
---
+++ logs/var.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+    description = "s3 bucket"
+}
+variable "vpc_id" {
+    description = "vpc"
+}
+variable "iam_role" {
+  description = "iam role"
+}
