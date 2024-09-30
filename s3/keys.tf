b04d29b6e45795e65d3677d74a22ca48e1054458
zablon <zablonoigo51@gmail.com>
2024-09-30 14:03:46 +0300

    enabling server side encryption with kms key

--- s3/keys.tf
+++ s3/keys.tf
@@ -10,3 +10,12 @@ resource "aws_kms_alias" "s3_bucket_kms_key_alias" {
   name          = "alias/s3_bucket_kms_key_alias"
   target_key_id = aws_kms_key.s3_bucket_kms_key.key_id
 }
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_encryption_with_kms_key" {
+  bucket = var.bucket_name.id
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3_bucket_kms_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
